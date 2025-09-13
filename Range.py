0d7487fdab2009e009c3437a8fd7ee30f5473751
estharkingson <estharkingson@gmail.com>
2025-09-13 16:03:44 +0530

    All loops Concept

---
+++ Range.py
@@ -0,0 +1,8 @@
+for i in range(4):
+    print(i) # Prints: 0 1 2 3
+
+for i in range(4, 8):
+    print(i) # Prints: 4 5 6 7
+
+for i in range(4, 10, 2):
+    print(i) # Prints: 4 6 8
---
+++ while_loop.py
@@ -0,0 +1,4 @@
+x = 0
+while x < 4:
+    print(x)
+    x += 1  # Shorthand for x = x + 1
